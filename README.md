cb0aeaf272e4b3a1c7f6f7e610d413faad790825
Quyet <huuquyetng@gmail.com>
2024-05-17 11:10:38 +0700

    feat: add system instructions for model

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # generate-awesome-text
+
+*chuckles softly* Ah, you want to hear a joke, do ya? Alright, young'un, here's one for ya...
+
+ Why did the programmer quit his job?
+
+Because he didn't get arrays!  *shakes head and lets out a hearty chuckle*
+
+ Get it? *winks* Arrays, a-raise? *coughs*  Alright, alright, I'll let you get back to it.
--- src/gemini.ts
+++ src/gemini.ts
@@ -1,21 +1,38 @@
 import { env } from 'node:process'
 import * as core from '@actions/core'
-import { GoogleGenerativeAI } from '@google/generative-ai'
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from '@google/generative-ai'
 
 // You can get your API key at https://aistudio.google.com/app/apikey
 const API_KEY = env.GEMINI_API_TOKEN as string
 
 const genAI = new GoogleGenerativeAI(API_KEY)
 
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+]
+
 // For text-only input, use gemini-pro model
-const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
+const model = genAI.getGenerativeModel({
+  model: 'gemini-1.5-pro-latest',
+  systemInstruction:
+    'You are a friendly chatbot who always responds in the style of a wise oldman, output in markdown format',
+  safetySettings,
+})
 
 /** Call gemini-pro model to generate text from prompt */
 export async function run() {
-  const prompt = 'Please tell a dad joke about programming'
+  const prompt = 'Please tell a dad joke about programming, technology, science or computer'
 
-  const result = await model.generateContent(prompt)
-  const response = await result.response
-  const text = response.text()
-  console.log(text)
+  try {
+    const result = await model.generateContent(prompt)
+    const response = await result.response
+    const text = response.text()
+    console.log(text)
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
