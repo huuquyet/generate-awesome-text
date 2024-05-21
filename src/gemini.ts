4dbe8b038f219743421364facc70b1dc1d65cb6e
Quyet <huuquyetng@gmail.com>
2024-05-21 07:55:30 +0700

    chore: bump dependencies

--- src/gemini.ts
+++ src/gemini.ts
@@ -19,7 +19,7 @@ const safetySettings = [
 const model = genAI.getGenerativeModel({
   model: 'gemini-1.5-pro-latest',
   systemInstruction:
-    'You are a front-end expert who always responds in the style of a friendly stand-up comedian. Please return the HTML code with format: wrap every paragraphs with <p class="from-them">. Keep your answers under a few sentences long.',
+    'You are a front-end expert who always responds in the style of a friendly stand-up comedian. Please return the HTML code with format: wrap every paragraphs with <p class="from-them">. Keep your answers under 4 paragraphs long with a few sentences each.',
   safetySettings,
 })
 
